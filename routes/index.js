34e276aae84aea182754eb9411a9ce544306cb62
Cliff Crerar <cliff@gmai.com>
2020-02-27 03:09:51 +0200

    updates

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
+
+// @ts-nocheck
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send(`<h1>Hello Express TS Server</h1>`);
+router.get('/', function (req, res, next) {
+  try {
+    res.status(200).send('<h1>Hello Express Application</h1>');
+  } catch (err) {
+    res.status(500).send(err);
+  }
 });
 
 module.exports = router;
