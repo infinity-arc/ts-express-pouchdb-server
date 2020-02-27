fe49586ea2e82dbcd077c5bc94ad5760e8f6cdb7
Cliff Crerar <cliff@gmai.com>
2020-02-27 04:26:51 +0200

    config tweaks

--- pouchdb/plugins.js
+++ pouchdb/plugins.js
@@ -1,7 +1,10 @@
 // @ts-nocheck
 
 const Pouchdb = require('pouchdb');
+const path = require('path');
 
 Pouchdb.plugin(require('pouchdb-upsert'))
+	
+defaults = Pouchdb.defaults({prefix: path.resolve()+'/data/'}) 
 
-module.exports = Pouchdb;
+module.exports = defaults;
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,14 @@ const express = require('express');
 const router = express.Router();
 const expressPouch = require('express-pouchdb');
 const Pouchdb = require('../pouchdb');
+const path = require('path');
 
-router.use(expressPouch(Pouchdb))
+console.log(path.resolve('data'));
+
+router.use(expressPouch(Pouchdb,{
+  configPath: path.resolve('./data/config.json'),
+  logPath: path.resolve('./data/logs/log.txt'),
+  mode: 'fullCouchDB',
+}))
 
 module.exports = router;
