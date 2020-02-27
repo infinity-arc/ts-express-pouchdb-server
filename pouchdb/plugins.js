ed1be62dac3ef101a6098f60c97bdb368f536b6c
Cliff Crerar <cliff@gmai.com>
2020-02-27 22:54:36 +0200

    updated for azure app engine

--- pouchdb/plugins.js
+++ pouchdb/plugins.js
@@ -2,9 +2,21 @@
 
 const Pouchdb = require('pouchdb');
 const path = require('path');
+const systemDb = require('pouchdb-system-db');
+const upsert = require('pouchdb-upsert');
+const levelDbAdapter = require('pouchdb-adapter-leveldb');
+const httpAdapter = require('pouchdb-adapter-http');
 
-Pouchdb.plugin(require('pouchdb-upsert'))
-	
-defaults = Pouchdb.defaults({prefix: path.resolve()+'/data/'}) 
+Pouchdb
+	.plugin(upsert)
+	.plugin(systemDb);
+// console.log('Pouchdb: ', Pouchdb.adapter);
+
+// Pouchdb.adapter('lev',httpAdapter,false);
+// Pouchdb.adapter('leveldb',levelDbAdapter,false);
+
+
+
+defaults = Pouchdb.defaults({ adapter: 'leveldb', prefix: path.resolve() + '/data/' });
 
 module.exports = defaults;
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,28 @@
-
 // @ts-nocheck
 const express = require('express');
+
 const router = express.Router();
+
 const expressPouch = require('express-pouchdb');
+
 const Pouchdb = require('../pouchdb');
+
 const path = require('path');
 
-router.use(expressPouch(Pouchdb,{
+const expressPouchApp = expressPouch(Pouchdb, {
+
   configPath: path.resolve('./data/config.json'),
+
   logPath: path.resolve('./data/logs/log.txt'),
-  mode: 'fullCouchDB',
-}))
+
+  mode: 'fullCouchDB'
+
+});
+
+// expressPouchApp.use(express.static(path.resolve('./data')));
+
+// expressPouchApp.use(express.static(path.resolve('./node_modules/pouchdb-fauxton')));
+
+router.use(expressPouchApp)
 
 module.exports = router;
